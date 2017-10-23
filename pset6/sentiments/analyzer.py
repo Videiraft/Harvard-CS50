f4b1b5edb160ba43f6dc13149cb41bd4f35bd467
Fábio Videira <F_Videira@MacBook-Pro-de-Fabio.local>
2017-10-23 15:56:22 +0100

    Update pset6/sentiments (Python/Web)

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -5,11 +5,30 @@ class Analyzer():
 
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
-
-        # TODO
+        self.positives = []
+        with open(positives) as lines:
+            for line in lines:
+                if not line.startswith(";"):
+                    self.positives.append(line.strip("\n"))
+        
+        self.negatives = []
+        with open(negatives) as lines:
+            for line in lines:
+                if not line.startswith(";"):
+                    self.negatives.append(line.strip("\n"))
+                    
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
-
-        # TODO
-        return 0
+        self.tokenizer = nltk.tokenize.TweetTokenizer()
+        tokens = self.tokenizer.tokenize(text)
+        
+        score = 0
+        for word in tokens:
+            for line in self.positives:
+                if word.lower() == line:
+                    score +=1
+            for line in self.negatives:
+                if word.lower() == line:
+                    score -= 1
+        return score
--- pset6/sentiments/application.py
+++ pset6/sentiments/application.py
@@ -1,4 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
+import os
+import sys
 
 import helpers
 from analyzer import Analyzer
@@ -18,10 +20,25 @@ def search():
         return redirect(url_for("index"))
 
     # get screen_name's tweets
-    tweets = helpers.get_user_timeline(screen_name)
+    tweets = helpers.get_user_timeline(screen_name, 100)
+    if not tweets:
+        return redirect(url_for("index"))
 
-    # TODO
-    positive, negative, neutral = 0.0, 0.0, 100.0
+    # instantiate analyzer
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+    analyzer = Analyzer(positives, negatives)
+    
+    # Analyzes the sentiment of each tweet
+    positive, negative, neutral = 0.0, 0.0, 0.0
+    for tweet in tweets:
+        score = analyzer.analyze(tweet)
+        if score > 0:
+            positive += 1
+        elif score < 0:
+            negative += 1
+        else:
+            neutral += 1
 
     # generate chart
     chart = helpers.chart(positive, negative, neutral)
